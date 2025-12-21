af8c90a81c4212cd416218af8adcdf4afb2ad4cd
Abhishek Tiwari <163037775+tabhishek07@users.noreply.github.com>
2025-12-21 14:09:00 +0530

    Remove sensitive and generated files from repo

--- backend/dist/middleware.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-export interface AuthRequest extends Request {
-    userId?: string;
-}
-export declare function authmiddleware(req: AuthRequest, res: Response, next: NextFunction): void | Response<any, Record<string, any>>;
-//# sourceMappingURL=middleware.d.ts.map
--- backend/dist/routes/content.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=content.js.map
